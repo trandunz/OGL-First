e218778dc988992b0fe8353beb73153e58e821df
trandunz <54924605+trandunz@users.noreply.github.com>
2021-12-09 18:27:33 +1300

    + Vector4 + Vector3 + Vector2

--- OGL/OGL/NumptyBehavior.h
+++ OGL/OGL/NumptyBehavior.h
@@ -6,6 +6,27 @@
 class NumptyBehavior
 {
 public:
+	struct Vector4
+	{
+		double x = 0.0;
+		double y = 0.0;
+		double z = 0.0;
+		double w = 0.0;
+	};
+
+	struct Vector3
+	{
+		double x = 0.0;
+		double y = 0.0;
+		double z = 0.0;
+	};
+
+	struct Vector2
+	{
+		double x = 0.0;
+		double y = 0.0;
+	};
+
 	template <class T>
 	static inline void CleanupPointer(T* _pointer)
 	{
