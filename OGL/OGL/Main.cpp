e4604a8b7ba7ab5a388c8e0913ad9a210f072e8b
Ashley <Ashley.rickit@gmail.com>
2021-12-09 19:05:19 +1300

    Shaderloader

--- OGL/OGL/Main.cpp
+++ OGL/OGL/Main.cpp
@@ -17,7 +17,6 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 
 int main()
 {
-	float ratio;
 	int width = 800, height = 800;
 
 	glfwSetErrorCallback(error_callback);
@@ -28,32 +27,47 @@ int main()
 		std::cout << "Failed to Initalise GLFW" << std::endl;
 	}
 
+	if (!glewInit())
+	{
+		std::cout << "Failed to Initalise GLEW" << std::endl;
+	}
+
+	// Set Context version to OpenGL 3.2
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 
-	m_RenderWindow = glfwCreateWindow(width, height, "My Title", NULL, NULL);
+	// Create Render Window
+	m_RenderWindow = glfwCreateWindow(width, height, "OGL-Project", NULL, NULL);
 
+	// Exit Safely if Render Window doesnt Initalise Properly
 	if (!m_RenderWindow)
 	{
 		glfwTerminate();
 		exit(EXIT_FAILURE);
 	}
 
+	// Set Key Input Callback
 	glfwSetKeyCallback(m_RenderWindow, key_callback);
 
 	glfwMakeContextCurrent(m_RenderWindow);
 
+	// Main Update & Render Loop
 	while (!glfwWindowShouldClose(m_RenderWindow))
 	{
-		
 
 		glfwGetFramebufferSize(m_RenderWindow, &width, &height);
-		ratio = width / (float)height;
 
 		glViewport(0, 0, width, height);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Swap Buffers at the start of each frame
 		glfwSwapBuffers(m_RenderWindow);
 		glfwPollEvents();
+
+		// Body 
+
+
+		
 
 	}
 
--- OGL/OGL/NumptyBehavior.h
+++ OGL/OGL/NumptyBehavior.h
@@ -1,7 +1,12 @@
 #pragma once
 
-#include <iostream>
+
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
+#include<fstream>
+#include<vector>
 
 class NumptyBehavior
 {
